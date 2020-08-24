1482302d0d09823faee8d2b531eac2fb79a27d64
FrtsVictor <victorfreitas.job@gmail.com>
2020-08-24 01:21:21 -0300

    Adicionando novas rotas HTTP @GetMapping("/about-us") @GetMapping("/size") @DeleteMapping("/clear")
    Adicionando Header para todos ResponseEntitys
    Reftorando funcionamento de métodos
    Refatorando mensagens output de erro/exito/criacao/deleçao/visualização

--- src/main/java/com/serratec/techbank1/controller/ContaController.java
+++ src/main/java/com/serratec/techbank1/controller/ContaController.java
@@ -2,7 +2,6 @@ package com.serratec.techbank1.controller;
 
 import java.util.List;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,70 +16,82 @@ import com.serratec.techbank1.exception.NumeroNaoEncontradoException;
 import com.serratec.techbank1.exception.ContaNullException;
 import com.serratec.techbank1.exception.ContaRepetidaException;
 import com.serratec.techbank1.exception.NumeroInvalidoException;
-import com.serratec.techbank1.exception.InvalidSaldoException;
+import com.serratec.techbank1.exception.SaldoInvalidoException;
+import com.serratec.techbank1.exception.ValorInvalidoException;
 import com.serratec.techbank1.model.Conta;
 import com.serratec.techbank1.service.ContaService;
-
-
 
 @RestController
 @RequestMapping("/conta")
 public class ContaController {
-	
-	
+
 	@Autowired
 	ContaService contaService;
-	
-	
+
+	@Autowired
+	AboutUsConfig aboutUsConfig;
+
+	@GetMapping("/about-us")
+	public ResponseEntity<AboutUsConfig> aboutUs() {
+		return new ResponseEntity<AboutUsConfig>(aboutUsConfig, contaService.Header(), HttpStatus.ACCEPTED);
+	}
+
 	@GetMapping
-	public ResponseEntity<List<Conta>> listarContas(){
+	public ResponseEntity<List<Conta>> listarContas() {
 		return new ResponseEntity<List<Conta>>(contaService.listarContas(), contaService.Header(), HttpStatus.OK);
 	}
-	
-	
+
+	@GetMapping("/size")
+	public ResponseEntity<String> contarLista() {
+		String tamanhoLista = "Contas no sistema: " + contaService.contarLista().toString();
+		return new ResponseEntity<String>(tamanhoLista, contaService.Header(), HttpStatus.OK);
+	}
+
 	@GetMapping("/{numero}")
-	public ResponseEntity<Conta> exibePorNumero(@PathVariable Integer numero) throws NumeroInvalidoException, NumeroNaoEncontradoException {
-		//contaService.exibirPorNumero(numero);		
+	public ResponseEntity<Conta> exibePorNumero(@PathVariable Integer numero)
+			throws NumeroInvalidoException, NumeroNaoEncontradoException {
+		// contaService.exibirPorNumero(numero);
 		return new ResponseEntity<Conta>(contaService.exibirPorNumero(numero), contaService.Header(), HttpStatus.OK);
 	}
 
-	
 	@PostMapping
-	public ResponseEntity<Conta> adicionarConta(Conta conta) throws ContaRepetidaException, ContaNullException{
-		//contaService.adicionarConta(conta);
-		//return ResponseEntity.status(HttpStatus.CREATED).body(conta);
+	public ResponseEntity<Conta> adicionarConta(Conta conta)
+			throws ContaRepetidaException, ContaNullException, NumeroInvalidoException, SaldoInvalidoException {
 		return new ResponseEntity<Conta>(contaService.adicionarConta(conta), contaService.Header(), HttpStatus.CREATED);
 	}
-		
-	
-	
+
 	@PutMapping
-	public ResponseEntity<Conta> atualizarConta(Conta conta) throws NumeroInvalidoException, NumeroNaoEncontradoException {
+	public ResponseEntity<Conta> atualizarConta(Conta conta)
+			throws NumeroInvalidoException, NumeroNaoEncontradoException, SaldoInvalidoException {
 		contaService.atualizarConta(conta);
-//		return ResponseEntity.status(HttpStatus.OK).body(conta);
-		return new ResponseEntity<Conta>(conta,contaService.Header(), HttpStatus.OK);
+		return new ResponseEntity<Conta>(conta, contaService.Header(), HttpStatus.OK);
 	}
-	
-	
-	@DeleteMapping("/{numero}")
-	public ResponseEntity<?> deletarConta(@PathVariable Integer numero) throws NumeroInvalidoException, NumeroNaoEncontradoException {
-		contaService.deletarConta(numero);
-		return new ResponseEntity<HttpStatus>(contaService.Header(), HttpStatus.OK);
-	}
-	
-	
+
 	@PutMapping("/{numero}/{tipo}={valor}")
-	public ResponseEntity<String> operacao(@PathVariable Integer numero,
-						   @PathVariable Double valor,
-						   @PathVariable String tipo) throws NumeroInvalidoException, NumeroNaoEncontradoException, InvalidSaldoException {
-		
-		contaService.operacao(numero, valor, tipo);	
-        String op = "Valor operacao = " + valor + "\nSaldo =  " + contaService.exibirPorNumero(numero).getSaldo();
-        //return ResponseEntity.status(HttpStatus.OK).body(op);
-        return new ResponseEntity<String>(op, contaService.Header(), HttpStatus.OK);
+	public ResponseEntity<String> operacao(@PathVariable Integer numero, @PathVariable Double valor,
+			@PathVariable String tipo) throws NumeroInvalidoException, NumeroNaoEncontradoException,
+			SaldoInvalidoException, ValorInvalidoException {
+
+		contaService.operacao(numero, valor, tipo);
+		String op = String.format("Tipo de operacao:%s\nValor da operacao:R$%.2f\n%s", tipo.toUpperCase(), valor,
+				contaService.exibirPorNumero(numero).toString());
+		return new ResponseEntity<String>(op, contaService.Header(), HttpStatus.OK);
 	}
-	
-	
-	
-	
+
+	@DeleteMapping("/{numero}")
+	public ResponseEntity<?> deletarConta(@PathVariable Integer numero)
+			throws NumeroInvalidoException, NumeroNaoEncontradoException {
+		Conta ctDeletada = contaService.exibirPorNumero(numero);
+		String cd = "Conta excluida com sucesso!\n" + ctDeletada.toString();
+		contaService.deletarConta(numero);
+		return new ResponseEntity<String>(cd, contaService.Header(), HttpStatus.OK);
+	}
+
+	@DeleteMapping("/clear")
+	public ResponseEntity<String> limparLista() {
+		contaService.limparLista();
+		String msg = "Contas excuidas com sucesso";
+		return new ResponseEntity<String>(msg, contaService.Header(), HttpStatus.OK);
+	}
+
 }
